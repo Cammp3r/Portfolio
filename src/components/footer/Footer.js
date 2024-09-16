695b77100f42b8c5422c9509f633fe3bf02602d8
Cammp3r <geogen1999@gmail.com>
2024-09-16 15:19:14 +0300

    20

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -9,8 +9,8 @@ const Footer = () => {
         <div className="container">
             <div className="footer__wrapper">
                 <ul className="social">  
-                    <li className="social__item"><a href="#!"><img src={discord} alt="Link"/></a></li>
-                    <li className="social__item"><a href="#!"><img src={github} alt="Link"/></a></li>
+                    <li className="social__item"><a href="https://discord.com/users/438712885854863360/"><img src={discord} alt="Link"/></a></li>
+                    <li className="social__item"><a href="https://github.com/Cammp3r"><img src={github} alt="Link"/></a></li>
                       </ul>
                 <div className="copyright">
                     <p>© 2024 </p>
